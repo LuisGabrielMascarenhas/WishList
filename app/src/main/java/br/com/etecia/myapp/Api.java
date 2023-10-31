ecbcb65282cb04d5fb2006fb65d46b11dfaaa86c
LuisGabrielMascarenhas <luis.gabriel.santos614@gmail.com>
2023-10-30 22:08:00 -0300

    correção no main

--- app/src/main/java/br/com/etecia/myapp/Api.java
+++ app/src/main/java/br/com/etecia/myapp/Api.java
@@ -1,7 +1,7 @@
 package br.com.etecia.myapp;
 
 public class Api {
-    private static final String ROOT_URL = "http://localhost/api/v1/Api.php?apicall=";
+    private static final String ROOT_URL = "http://192.168.56.1/api/v1/Api.php?apicall=";
     public static final String URL_CREATE_PRODUTO = ROOT_URL + "criarProduto";
     public static final String URL_READ_PRODUTO = ROOT_URL + "getProduto";
     public static final String URL_UPDATE_PRODUTO = ROOT_URL + "updateProduto";
--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -16,7 +16,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.RatingBar;
@@ -30,13 +29,13 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-//TODO Concertar problema com criação da Card
+
 public class MainActivity extends AppCompatActivity {
     private static final int CODE_GET_REQUEST = 1024;
     private static final int CODE_POST_REQUEST = 1025;
 
     ImageView imageViewProduto;
-    EditText editTextNome,editTextValor,editId;
+    EditText editTextNome, editTextValor, editId;
     RatingBar ratingBar;
     ProgressBar progressBar;
     ListView listView;
@@ -53,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
         editTextNome = findViewById(R.id.editTextName);
         editTextValor = findViewById(R.id.editTextValor);
-        editId = findViewById(R.id.editId);
+        //editId = findViewById(R.id.editId);
 
         ratingBar = findViewById(R.id.ratingBar);
 
@@ -68,34 +67,35 @@ public class MainActivity extends AppCompatActivity {
         btnAddUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (isUpdating){
+                if (isUpdating) {
                     updateProduto();
-                }else {
+                } else {
                     criarProduto();
                 }
             }
         });
         getProdutos();
     }
-    private void criarProduto(){
-        String id = editId.getText().toString();
+
+    private void criarProduto() {
+        //String id = editId.getText().toString();
         String nome = editTextNome.getText().toString().trim();
-        float valor = (float) editTextValor.getTextAlignment();
+        float valor = Float.parseFloat(editTextValor.getText().toString());
 
         int rating = (int) ratingBar.getRating();
 
-        if (TextUtils.isEmpty(nome)){
-            editTextNome.setError("Por favor entre com o nome");
+        if (TextUtils.isEmpty(nome)) {
+            editTextNome.setError("Por favor, insira o nome");
             editTextNome.requestFocus();
             return;
         }
         HashMap<String, String> params = new HashMap<>();
-        params.put("id",id);
-        params.put("nome",nome);
+        //params.put("id", id);
+        params.put("nome", nome);
         params.put("valor", String.valueOf(valor));
-        params.put("rating",String.valueOf(rating));
+        params.put("rating", String.valueOf(rating));
 
-        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_CREATE_PRODUTO,params,CODE_POST_REQUEST);
+        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_CREATE_PRODUTO, params, CODE_POST_REQUEST);
         request.execute();
 
         btnAddUpdate.setText("Adicionar");
@@ -105,74 +105,77 @@ public class MainActivity extends AppCompatActivity {
         ratingBar.setRating(0);
 
         isUpdating = false;
-
     }
-    private void deletarProduto(int id){
-        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_DELETE_PRODUTO + id,null,CODE_GET_REQUEST);
+
+    private void deletarProduto(int id) {
+        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_DELETE_PRODUTO + id, null, CODE_GET_REQUEST);
         request.execute();
     }
 
-    private void getProdutos(){
-        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_READ_PRODUTO,null, CODE_GET_REQUEST);
+    private void getProdutos() {
+        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_READ_PRODUTO, null, CODE_GET_REQUEST);
         request.execute();
     }
 
-    private void updateProduto(){
-
+    private void updateProduto() {
         String nome = editTextNome.getText().toString();
-        String valor = String.valueOf((float) editTextValor.getTextAlignment());
+        String valor = String.valueOf(Float.parseFloat(editTextValor.getText().toString()));
 
         int rating = (int) ratingBar.getRating();
 
-        if (TextUtils.isEmpty(nome)){
-            editTextNome.setError("Por favor entre com o nome");
+        if (TextUtils.isEmpty(nome)) {
+            editTextNome.setError("Por favor, insira o n    ome");
             editTextNome.requestFocus();
             return;
         }
-        if (TextUtils.isEmpty(valor)){
-            editTextValor.setError("Coloque o valor");
+        if (TextUtils.isEmpty(valor)) {
+            editTextValor.setError("Insira o valor");
             editTextValor.requestFocus();
             return;
         }
-        HashMap<String,String> params = new HashMap<>();
-        params.put("nome",nome);
-        params.put("valor",valor);
+        HashMap<String, String> params = new HashMap<>();
+        params.put("nome", nome);
+        params.put("valor", valor);
         params.put("rating", String.valueOf(rating));
 
-        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_UPDATE_PRODUTO,params,CODE_POST_REQUEST);
-
+        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_UPDATE_PRODUTO, params, CODE_POST_REQUEST);
+        request.execute();
     }
-    private void refreshProdutoList(JSONArray wishlists) throws JSONException{
+
+    private void refreshProdutoList(JSONArray wishlists) throws JSONException {
         wishList.clear();
 
-        for (int i = 0; i <wishlists.length();i++){
+        for (int i = 0; i < wishlists.length(); i++) {
             JSONObject obj = wishlists.getJSONObject(i);
 
             wishList.add(new Wish(
-               obj.getInt("id"),
-               obj.getString("nome"),
-               obj.getInt("valor"),
+                    obj.getInt("id"),
+                    obj.getString("nome"),
+                    obj.getInt("valor"),
                     obj.getInt("rating")
             ));
         }
         WishAdapter adapter = new WishAdapter(wishList);
         listView.setAdapter(adapter);
-
     }
-    private class PerformNetworkRequest extends AsyncTask<Void, Void, String>{
+
+    private class PerformNetworkRequest extends AsyncTask<Void, Void, String> {
         String url;
-        HashMap<String,String> params;
+        HashMap<String, String> params;
         int requestCode;
+
         PerformNetworkRequest(String url, HashMap<String, String> params, int requestCode) {
             this.url = url;
             this.params = params;
             this.requestCode = requestCode;
         }
+
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
             progressBar.setVisibility(View.VISIBLE);
         }
+
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
@@ -187,21 +190,21 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+
         @Override
         protected String doInBackground(Void... voids) {
             RequestHandler requestHandler = new RequestHandler();
 
             if (requestCode == CODE_POST_REQUEST)
-                return requestHandler.sendPostRequest(url,params);
+                return requestHandler.sendPostRequest(url, params);
 
             if (requestCode == CODE_GET_REQUEST)
                 return requestHandler.sendGetRequest(url);
 
-
-
             return null;
         }
     }
+
     class WishAdapter extends ArrayAdapter<Wish> {
         List<Wish> wishList;
 
@@ -209,6 +212,7 @@ public class MainActivity extends AppCompatActivity {
             super(MainActivity.this, R.layout.wish_list_layout, wishList);
             this.wishList = wishList;
         }
+
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             LayoutInflater inflater = getLayoutInflater();
@@ -228,33 +232,35 @@ public class MainActivity extends AppCompatActivity {
                     isUpdating = true;
                     editId.setText(String.valueOf(wish.getId()));
                     editTextNome.setText(wish.getNome());
-                    editTextValor.setText(wish.getValor());
+                    editTextValor.setText(String.valueOf(wish.getValor()));
                     ratingBar.setRating(wish.getRating());
                     btnAddUpdate.setText("Alterar");
                 }
             });
+
             txtViewDelete.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Apagar " + wish.getNome())
-                        .setMessage("Tem certeza que deseja excluir?")
-                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialogInterface, int which) {
-                                deletarProduto(wish.getId());
-                            }
-                        })
-                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-
-                            public void onClick(DialogInterface dialogInterface, int which) {
-
-                            }
-                        })
-                        .setIcon(android.R.drawable.ic_dialog_alert)
-                        .show();
+                    builder.setTitle("Apagar " + wish.getNome())
+                            .setMessage("Tem certeza que deseja excluir?")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialogInterface, int which) {
+                                    deletarProduto(wish.getId());
+                                }
+                            })
+                            .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialogInterface, int which) {
+                                    // Faça algo se o usuário optar por não excluir
+                                }
+                            })
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
                 }
             });
-return listViewItem;
+            return listViewItem;
+        }
+
+    }
 }
-}
-}
+
